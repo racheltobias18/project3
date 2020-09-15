db63835666469020c45c8ef3908fcfa7818ec2e9
racheltobias18 <71040579+racheltobias18@users.noreply.github.com>
2020-09-15 12:20:36 -0500

    Coded the total requests for the timeframe

--- project3.py
+++ project3.py
@@ -1,2 +1,13 @@
 import urllib.request
 urllib.request.urlretrieve('https://s3.amazonaws.com/tcmg476/http_access_log','serverlogs.txt')
+
+file = open("serverlogs.txt", "r")
+total_requests = 0
+
+for aline in file:
+    line = aline.strip()
+    words = line.split
+    total_requests += 1
+file.close()
+
+print("The number of requests is", total_requests)
