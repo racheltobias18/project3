17e4696c4a0df689d336fa5fcbf0842dea5695e5
racheltobias18 <71040579+racheltobias18@users.noreply.github.com>
2020-09-15 10:33:40 -0500

    Update project3.py

--- project3.py
+++ project3.py
@@ -1,5 +1,2 @@
-import requests
-
-url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-r = requests.get(url, allow_redirects=True)
-open('serverlog.http', 'wb').write(r.content)
+import urllib.request
+urllib.request.urlretrieve('https://s3.amazonaws.com/tcmg476/http_access_log','serverlogs.txt')
